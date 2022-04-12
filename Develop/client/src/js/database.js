9d2be22035234386f476001da26a6e6a2627d331
Liam Ok <liamokane19@gmail.com>
2022-04-12 15:05:07 +1000

    database update for get and put methods with the right database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -3,20 +3,19 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('editor')) {
+        console.log('editor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('editor', { keyPath: 'id', autoIncrement: true });
+      console.log('editor database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
 //connect to DB and the version we want to use
-const editorDb = await openDB('editor', 1);
+const editorDb = await openDB('jate', 1);
 //create transation and specify DB
 const tx = editorDb.transaction('editor', 'readwrite');
 //open up desired objectSTore
@@ -31,9 +30,8 @@ console.log('🚀 - data saved to the database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
   // Create a connection to the database database and version we want to use.
-  const editorDb = await openDB('editor', 1);
+  const editorDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
   const tx = editorDb.transaction('editor', 'readonly');
